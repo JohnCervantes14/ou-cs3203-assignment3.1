1c2690e4cea24ee9953dcf3a2ccf53280b5c90a2
john-cervantes <john.f.cervantes-1@ou.edu>
2023-02-20 10:24:38 -0600

    step 2 multiply function

--- CS3203_hw3/src/Main.java
+++ CS3203_hw3/src/Main.java
@@ -1,10 +1,20 @@
 
 public class Main {
+	
+	
 	public static int ListSum(int[] array1) {
 		int sum = 0;
 		for(int i = 0; i < array1.length; ++i) {
 			sum += array1[i];
 		}
 		return sum;
+	}
+	
+	public static int Multiply(int[] list) {
+		int product = 1;
+		for(int i = 0; i < list.length; ++i) {
+			product = list[i] * product;
+		}
+		return product;
 	}
 }
